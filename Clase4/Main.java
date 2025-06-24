f6feec8bbe50eda6b1c866cae926f1bc7596b73f
EseSx <santiagoeseiza10@gmail.com>
2025-06-24 20:13:11 -0300

    Nose

--- Clase4/Main.java
+++ Clase4/Main.java
@@ -1,8 +1,7 @@
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
         Alumno santi = new Alumno(7.0f, 9.1f);
-        
+
         System.out.println(santi);
         System.out.println("-------------");
         System.out.println(santi.promediar());
